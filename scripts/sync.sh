9f6e2c3b9550e94d014596ec8bf33bdb68f9cf78
unparalleled-js <yingthi@live.com>
2020-08-01 11:04:40 -0500

    Update sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 
-if cd ..
-then
-npx webpack
+pushd $PORTFOLIO_PROJ_PATH
+if [ -z ${PORTFOLIO_SITE_PATH+x} ]; then
+  echo "Environment variable \$PORTFOLIO_SITE_PATH is unset. Please point it to local site."
+  exit 1
 else
-    echo "Failure to cd for webpack."
+  echo "Local Site Path: ${PORTFOLIO_SITE_PATH}"
 fi
-if cd build
-then
-    gsutil rsync -R . gs://www.juliyasmith.com
+
+if [ -z ${PORTFOLIO_PROJ_PATH+x} ]; then
+  echo "Environment variable \$PORTFOLIO_PROJ_PATH is unset. Please point it to local site."
+  exit 1
 else
-    echo "Failure to cd for gsutil rsync."
+  echo "Local Project Path: ${PORTFOLIO_PROJ_PATH}"
 fi
-echo "Synchronization complete."
+
+# BUILD
+rm -rf build &> /dev/null
+npm run build
+
+if [ $? -eq 0 ]; then
+    echo BUILD DONE
+else
+    echo BUILD FAILED
+    exit 1
+fi
+
+pushd $PORTFOLIO_SITE_PATH;
+rm -rf css &> /dev/null
+rm -rf images &> /dev/null
+rm -rf js &> /dev/null
+rm vendors* &> /dev/null
+rm index.html &> /dev/null
+mkdir css
+mkdir images
+mkdir js
+popd
+cp -R build/css/ $PORTFOLIO_SITE_PATH/css
+cp -R build/js/ $PORTFOLIO_SITE_PATH/js
+cp -R build/images/ $PORTFOLIO_SITE_PATH/images
+cp build/index.html $PORTFOLIO_SITE_PATH
+cp build/vendors* $PORTFOLIO_SITE_PATH
+pushd $PORTFOLIO_SITE_PATH
+git add .
+git commit -m "Update"
+git push -f
+popd
+popd
+echo DONE
