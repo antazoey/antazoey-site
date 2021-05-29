52451907ca2b1de92b6a393cd4ab095021053d93
unparalleled-js <yingthi@live.com>
2021-05-28 19:13:48 -0500

    cleans

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,32 @@
 # ./deploy.sh or `npm run deploy`.
 #
 
-#set -eu pipefail
+set -eu
 
 
 main() {
   # Should be the directory of the github site repo (the one that hosts the build files).
   deploy_dir="${1:?Missing required parameter 'deploy_dir' at param1.}"
 
+  npm run clean
   npm run build
 
   echo "Waiting for /build to exist..."
-  sleep 10
+  sleep 25
 
-  mv build/* $deploy_dir
-  cd $deploy_dir
+  # Remove files that will be regenerated with different names.
+  rm ${deploy_dir}/vendors~pdfjsWorker.*
+
+  cp -r build/* "${deploy_dir}"
+  pushd "${deploy_dir}"
+  git pull
   git add --all
+
+  git status  # for logging purposes
+
   git commit -m "Deploy"
   git push
+  popd
 }
 
 main "$@" $PORTFOLIO_SITE_PATH
